479e303129615297a0cfaf5f50f1351c244edd09
Ksenia <ksenia.barteva@gmail.com>
2023-01-22 17:02:54 +0700

    Lesson

---
+++ Lesson/program.java
@@ -0,0 +1,76 @@
+package Lesson;
+
+/**
+ * program
+ */
+/*
+public class program {
+
+    public static void main(String[] args) {
+        short age = 10;
+        int salary = 123456;
+        float e = 2.7f;
+        double pi = 3.1415;
+        System.out.println(age); //10
+        System.out.println(salary); //123456
+        System.out.println(e); // 2.7
+        System.out.println(pi); //3.1415
+    }
+}
+*/
+/**
+ * program
+ */
+/*
+public class program {
+
+    public static void main(String[] args) {
+        char ch = '1';
+        System.out.println(Character.isDigit(ch)); // true
+        ch = 'a';
+        System.out.println(Character.isDigit(ch)); // false 
+    }
+}
+*/
+/**
+ * program
+ */
+/*
+public class program {
+
+    public static void main(String[] args) {
+        boolean flag1 = 123 <= 234;
+        System.out.println(flag1); // true
+        boolean flag2 = 123 >= 234 || flag1;
+        System.out.println(flag2); // true
+        boolean flag3 = flag1 ^ flag2;
+        System.out.println(flag3); // false
+    }
+}
+*/
+
+/**
+ * program
+ */
+/*
+public class program {
+
+    public static void main(String[] args) {
+        String s = "qwer";
+        System.out.println(s.charAt(2)); //s.length()
+    }
+}
+*/
+/**
+ * program
+ */
+public class program {
+
+    public static void main(String[] args) {
+        int a = 123;
+        a = --a - a--;
+
+        //System.out.println(a--);
+        System.out.println(a);
+    }
+}
